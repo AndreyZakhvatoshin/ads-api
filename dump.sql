def8828c51446e07a88934d4dea8edea397b6c05
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-07-13 22:41:53 +0500

    create dump

---
+++ dump.sql
@@ -0,0 +1,9 @@
+CREATE TABLE app.ads (
+  id INT auto_increment NOT NULL,
+  text varchar(100) NOT NULL,
+  price DECIMAL(6, 2) NOT NULL,
+  `limit` INT NOT NULL,
+  banner varchar(100) NOT NULL,
+  show_count INT NULL,
+  CONSTRAINT ads_PK PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
