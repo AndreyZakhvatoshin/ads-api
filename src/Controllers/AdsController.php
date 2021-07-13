603d16e1ec127d928a1e437170e26c2380b29627
AndreyZakhvatoshin <mastetandron@gmail.com>
2021-07-13 16:48:54 +0500

    add response

--- src/Controllers/AdsController.php
+++ src/Controllers/AdsController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Components\Database;
 use App\Components\Validator;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdsController
@@ -14,17 +15,30 @@ class AdsController
         var_dump($db);
     }
 
+    /**
+     * Create new ads
+     *
+     * @return void
+     */
     public function store()
     {
         $request = Request::createFromGlobals();
         $validator = new Validator();
         if (!$validator->validateAds($request->request->all())) {
-            header('Content-Type: application/json');
-            echo json_encode(['errors' => $validator->getErrors(), 'code' => 400, 'data' => []]);
+            $response = new JsonResponse(['errors' => $validator->getErrors(), 'code' => 400, 'data' => []]);
+            return $response->send();
         }
-        
+        $db = new Database();
+        $db->createAds('ads', $request->request->all());
+        $response = new JsonResponse(["message" => "OK",
+            "code" => 200,
+            "data" => [
+                "id" => $db->lastId,
+                "text" => $request->request->get('text'),
+                "banner" => $request->request->get('banner')],
+        ]);
+        return $response->send();
     }
-
     public function update(int $id)
     {
         var_dump(dirname(dirname(__DIR__)));
