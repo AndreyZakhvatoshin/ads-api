2805aa3288b0f1bc950e5a33363d20a2518726f2
AndreyZakhvatoshin <mastetandron@gmail.com>
2021-07-13 16:00:26 +0500

    add create table

--- src/Components/Database.php
+++ src/Components/Database.php
@@ -21,6 +21,10 @@ class Database
         return new \PDO("{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']}", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
     }
 
+    public function createTable()
+    {
+        
+    }
     public function createAds($table, $data)
     {
         
