eebeeb0311f1c2dcc5ae0f58457145511a7ea1cf
AndreyZakhvatoshin <mastetandron@gmail.com>
2021-07-14 10:49:33 +0500

    Add comments

--- src/Controllers/AdsController.php
+++ src/Controllers/AdsController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 class AdsController
 {
     /**
-     * Create new ads
+     * Create new advertisements
      *
      * @return void
      */
@@ -30,19 +30,59 @@ class AdsController
             "data" => [
                 "id" => $db->lastId,
                 "text" => $request->request->get('text'),
-                "banner" => $request->request->get('banner')
+                "banner" => $request->request->get('banner'),
             ],
         ]);
         return $response->send();
     }
+
+    /**
+     * Updating advertisements
+     *
+     * @param integer $id
+     * @return void
+     */
     public function update(int $id)
     {
-        var_dump(dirname(dirname(__DIR__)));
+        $request = Request::createFromGlobals();
+        $validator = new Validator();
+        if (!$validator->validateUpdateAds($request->request->all())) {
+            $response = new JsonResponse(['message' => $validator->getErrors(), 'code' => 400, 'data' => []]);
+            return $response->send();
+        }
+        $db = new Database();
+        $db->updateAds('ads', $id, $request->request->all());
+        $ads = $db->getUpdatingAds();
+        $response = new JsonResponse([
+            "message" => "OK",
+            "code" => 200,
+            "data" => [
+                "id" => $ads['id'],
+                "text" => $ads['text'],
+                "banner" => $ads['banner'],
+            ],
+        ]);
+        return $response->send();
     }
 
+    /**
+     * Show advertisements with max price
+     *
+     * @return void
+     */
     public function relevant()
     {
         $db = new Database();
-        $data = $db->relevant('ads');
+        $ads = $db->relevant('ads');
+        $response = new JsonResponse([
+            "message" => "OK",
+            "code" => 200,
+            "data" => [
+                "id" => $ads['id'],
+                "text" => $ads['text'],
+                "banner" => $ads['banner'],
+            ],
+        ]);
+        return $response->send();
     }
 }
