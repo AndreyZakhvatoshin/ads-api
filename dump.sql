398829f4e09501db8aa8b68a2bb00cc3ad1a8ff8
AndreyZakhvatoshin <mastetandron@gmail.com>
2021-07-14 10:48:02 +0500

    change sql query

--- dump.sql
+++ dump.sql
@@ -4,6 +4,6 @@ CREATE TABLE app.ads (
   price DECIMAL(6, 2) NOT NULL,
   `limit` INT NOT NULL,
   banner varchar(100) NOT NULL,
-  show_count INT NULL,
+  show_count INT NOT NULL DEFAULT 0,
   CONSTRAINT ads_PK PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
